dc76881b5860da46545a20d2487bf95f8f39048c
Aisha <aishaar283@gmail.com>
2021-02-25 13:23:46 +0400

    product list

---
+++ product.js
@@ -0,0 +1,29 @@
+let products = [
+    {
+        "id": 1001,
+        "title": "Cat Food, 25lb bag",
+        "description": "A 25 pound bag of <em>irrestible</em>, origanic goodness for your cat.",
+        "price": 20.00,
+        "image": "images/cat.jpg",
+        "availableInventory": 10,
+        "rating" : 2
+    },
+    {
+        "id": 1002,
+        "title": "Dog Food, 30lb bag",
+        "description": "A 25 pound bag of <em>irrestible</em>, origanic goodness for your dog.",
+        "price": 30.00,
+        "image": "images/dog.jpeg",
+        "availableInventory": 10,
+        "rating" : 3
+    },
+    {
+        "id": 1003,
+        "title": "Bird Food, 15lb bag",
+        "description": "A 25 pound bag of <em>irrestible</em>, origanic goodness for your bird.",
+        "price": 15.00,
+        "image": "images/bird.jpeg",
+        "availableInventory": 10,
+        "rating" : 4
+    }
+]
