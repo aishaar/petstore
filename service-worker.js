f128fbe6349d8419683ae2eb059d6859104eb1c8
Aisha <aishaar283@gmail.com>
2021-02-25 14:32:06 +0400

    added fetch code 1

--- service-worker.js
+++ service-worker.js
@@ -17,4 +17,15 @@ self.addEventListener('install', (e) => {
             return cache.addAll(cacheFiles);
         })
    );
-})
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith(
+        //check if the cache has the file
+        caches.match(e.request).then(function (r) {
+            console.log('[Service Worker] Fetching resources:' + e.request.url );
+            //'r' is the matching file if it exists in the cache
+            return r
+        })
+    );
+});
